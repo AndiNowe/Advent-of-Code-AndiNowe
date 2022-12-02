002ea92a50d77bb690b1cc71f0d5dab2d55ce1a0
Andi <andrea.bonet01@gmail.com>
2022-12-02 23:42:23 +0100

    Solve day 1

---
+++ Day 1/day1a.ts
@@ -0,0 +1,20 @@
+import * as fs from 'fs';
+
+let data: Array<number> = [];
+
+let calories: number = 0;
+
+const allFileContents = fs.readFileSync('data/data.txt', 'utf-8');
+allFileContents.split(/\r?\n/).forEach(l =>  {
+    if (l.length > 0) {
+        calories += parseInt(l);
+    } else {
+        data.push(calories);
+        calories = 0;
+    }
+});
+data.push(calories);
+
+console.log(Math.max(...data));
+
+
