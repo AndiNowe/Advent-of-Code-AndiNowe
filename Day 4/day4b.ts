30f38205aeba77554bbdb2b2d2e0a5a9ecea1eb8
Andi <andrea.bonet01@gmail.com>
2022-12-08 00:35:31 +0100

    Solve day 4 part b

---
+++ Day 4/day4b.ts
@@ -0,0 +1,26 @@
+import * as fs from 'fs';
+
+let counter: number = 0;
+
+const allFileContents = fs.readFileSync('data/data.txt', 'utf-8');
+allFileContents.split(/\r?\n/).forEach(l =>  {
+    arrangeIntervals(l);
+});
+
+console.log(counter);
+
+function arrangeIntervals(line: string): void {
+
+    let twoIntervalArray: Array<string> = line.split(",");
+    let firstInterval: Array<string> = twoIntervalArray[0].split("-");
+    let secondInterval: Array<string> = twoIntervalArray[1].split("-");
+
+    let x1: number = parseInt(firstInterval[0]);
+    let x2: number = parseInt(firstInterval[1]);
+    let y1: number = parseInt(secondInterval[0]);
+    let y2: number = parseInt(secondInterval[1]);
+
+    if (Math.max(x1, y1) - Math.min(x2, y2) <= 0) {
+        counter++;
+    }
+};
