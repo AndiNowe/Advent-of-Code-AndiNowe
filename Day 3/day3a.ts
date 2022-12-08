3b1fff76fdb819a948717b4a684866d86454b13c
Andi <andrea.bonet01@gmail.com>
2022-12-08 01:42:11 +0100

    Update day 3a

--- Day 3/day3a.ts
+++ Day 3/day3a.ts
@@ -2,33 +2,28 @@ import * as fs from 'fs';
 
 const prioritiesArray: Array<string> = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 let listOfDuplicates: Array<string> = [];
-let currentRuckSackItems: Array<string> = [];
 
 const allFileContents = fs.readFileSync('data/data.txt', 'utf-8');
 allFileContents.split(/\r?\n/).forEach(l =>  {
-    listRuckSackItems(getItemsRight(l), getItemsLeft(l));
-    listOfDuplicates.push(...currentRuckSackItems);
-    currentRuckSackItems = [];
+    listOfDuplicates.push(listRuckSackItems(getItemsRight(l), getItemsLeft(l)));
 });
 
 console.log(sumPriorities(listOfDuplicates, prioritiesArray));
 
-function listRuckSackItems(itemsRight: string, itemsLeft: string): Array<string> {
+function listRuckSackItems(itemsRight: string, itemsLeft: string): string{
 
     for (let i = 0; i < itemsRight.length; i++) {
         for (let j = 0; j < itemsLeft.length; j++) {
-            if (itemsRight[i] === itemsLeft[j] && !currentRuckSackItems.includes(itemsRight[i])) {
-                currentRuckSackItems.push(itemsRight[i]);
-                break;
+            if (itemsRight[i] === itemsLeft[j]) {
+                return itemsRight[i];
             }
         }
     }
-    return currentRuckSackItems;
+    return null;
 };
 
 function sumPriorities(listOfDuplicates: Array<string>, prioritiesArray: Array<string>): number {    
     let totalPriority: number = 0;
-    console.log("UNIQUE " , listOfDuplicates);
 
     for (let i = 0; i < listOfDuplicates.length; i++) {
         for (let j = 0; j < prioritiesArray.length; j++) {
@@ -49,4 +44,3 @@ function getItemsLeft(items: string): string {
     let itemsLeft: string = items.slice((items.length)/2, items.length);;
     return itemsLeft;
 };
-
